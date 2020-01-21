4114f054204027fc07bd75767e3b83593cf2c85c
JerryTang <tt-tjw@qq.com>
2020-01-21 20:08:41 +0800

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginRoutingModule } from './login/login-routing.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: "full" },
+  {
+    path: 'login', loadChildren: "./login/login.module#LoginModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,17 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
 
-
-const MAT_MODULE = [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule]
+const MAT_MODULE = [
+  MatSidenavModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatCardModule
+]
 @NgModule({
   declarations: [],
   imports: [CommonModule, ...MAT_MODULE],
